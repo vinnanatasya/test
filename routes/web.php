7439736f52fa43a232f7931668ad3f39f0721766
vinnanatasya <natasyavinnaa@gmail.com>
2023-05-16 19:49:37 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->middleware('auth');
+Route::get('resto/home',
+[App\Http\Controllers\resto\HomeController::class, 'index'])
+->middleware('can:isResto');
+Route::get('kurir/home',
+[App\Http\Controllers\kurir\HomeController::class, 'index'])
+->middleware('can:isKurir');
+Route::get('konsumen/home',
+[App\Http\Controllers\konsumen\HomeController::class, 'index'])
+->middleware('can:isKonsumen');
+Route::get('resto/user',
+[App\Http\Controllers\resto\UserController::class, 'index'])
+->middleware('can:isResto');
+Route::get('resto/user/create',
+[App\Http\Controllers\resto\UserController::class, 'create'])
+->middleware('can:isResto');
+Route::post('resto/user/store',
+[App\Http\Controllers\resto\UserController::class, 'store'])
+->middleware('can:isResto');
+Route::get('resto/user/edit/{id}',
+[App\Http\Controllers\resto\UserController::class, 'edit'])
+->middleware('can:isResto');
+Route::post('resto/user/update/{id}',
+[App\Http\Controllers\resto\UserController::class, 'update'])
+->middleware('can:isResto');
+Route::post('resto/user/destroy/{id}',
+[App\Http\Controllers\resto\UserController::class, 'destroy'])
+->middleware('can:isResto');
